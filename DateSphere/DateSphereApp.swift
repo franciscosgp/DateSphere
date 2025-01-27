ea7f6b17968ca5b56b74d56d60b2dd4b2ac10188
Francisco Solano Gómez Pino <franciscosgp@ext.inditex.com>
2025-01-27 09:38:06 +0100

    Initial Commit

---
+++ DateSphere/DateSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateSphereApp.swift
+//  DateSphere
+//
+//  Created by Francisco Gómez Pino on 27/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DateSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DateSphereTests/DateSphereTests.swift
@@ -0,0 +1,17 @@
+//
+//  DateSphereTests.swift
+//  DateSphereTests
+//
+//  Created by Francisco Gómez Pino on 27/1/25.
+//
+
+import Testing
+@testable import DateSphere
+
+struct DateSphereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
